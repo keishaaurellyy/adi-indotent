f3a745eee3882ddd26f234033e24a72e88d72e01
Keisha <230521850+keishaaurellyy@users.noreply.github.com>
2026-09-12 14:31:35 +0700

    fix(be): cleanup backend strapi

--- backend/src/api/peralatan-pendukung/routes/peralatan-pendukung.ts
+++
@@ -1,7 +0,0 @@
-/**
- * peralatan-pendukung router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::peralatan-pendukung.peralatan-pendukung');
--- backend/src/api/peralatan-pendukung/services/peralatan-pendukung.ts
+++
@@ -1,7 +0,0 @@
-/**
- * peralatan-pendukung service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::peralatan-pendukung.peralatan-pendukung');
